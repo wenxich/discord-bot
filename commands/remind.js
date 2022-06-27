d995461a36e7d4108bed60074a935b9af9c8049f
wenxich <wenxi.c.chen@gmail.com>
2022-06-27 17:45:17 -0400

    Edited remind command.

--- commands/remind.js
+++ commands/remind.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('remind')
         .setDescription('Set a reminder!'),
     async execute(interaction) {
-        await interaction.reply(`>>> To set a reminder, reply with "(the title of your reminder) (time interval)".\nFor example, to set a wake-up alarm in 2 hours, reply "(WAKE UP!) (2h)".`);
+        await interaction.reply(`>>> To set a reminder, reply with __the title of your reminder in quotation marks__ and then a __time interval__ (a number + either D, H, or S for days, hours, or seconds).\nFor example, to set a wake-up alarm in 2 hours, you could reply **"WAKE UP!" 2H**.`);
     },
 };
